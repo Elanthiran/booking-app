962c4c1e9ded72b513abeed60e4f7fab2a626afb
Elanthiran <elanthirank@gmail.com>
2024-09-10 21:45:54 +0530

    updated

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ const app = express();
 app.use(express.json());
 app.use('/',AppRoutes)
 
+app.use("/", (req, res) => {
+  res.status(200).send("welcome ");
+});
 
 app.listen(8000, ()=> console.log("Server listening to port 8000"));
